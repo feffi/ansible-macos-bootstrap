50cf5f6b7e2985b3001b17e497c4b0edc39e27ce
feffi <feffi@feffi.org>
2017-09-25 23:42:38 +0200

    full test

--- bootstrap.sh
+++ bootstrap.sh
@@ -211,4 +211,15 @@ install_clt
 install_pip
 install_ansible
 $(which ansible-galaxy) install -f -r requirements.yml -p roles
-$(which ansible-playbook) -i "localhost" bootstrap.yml
+$(which ansible-playbook) -i "localhost" bootstrap.yml --extra-vars='cli_path: [ $1 ]'
+
+###############################################################################
+# Kill all affected applications                                              #
+###############################################################################
+
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+  "Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
+  "Terminal" "Transmission" "Twitter" "iCal"; do
+  killall "${app}" > /dev/null 2>&1
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
